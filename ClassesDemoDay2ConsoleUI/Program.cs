9d4f8fcad7fff9111dc70a8e4047c2378e50f63d
ElsaV22 <elsa22@att.net>
2020-07-29 13:24:18 -0500

    added classes

--- ClassesDemoDay2ConsoleUI/Program.cs
+++ ClassesDemoDay2ConsoleUI/Program.cs
@@ -6,7 +6,45 @@ namespace ClassesDemoDay2ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Instanciating a customer = creating a variable of the Customer data type
+            Customer customer1 = new Customer();
+
+            //getting user input
+            Console.WriteLine("Please give me your first name:");
+            customer1.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Please give me your last name:");
+            customer1.LastName = Console.ReadLine();
+
+            //printing customer info
+            Console.WriteLine($"Hello, {customer1.FirstName} {customer1.LastName}");
+            Console.WriteLine("Please enter your age:");
+            customer1.Age = int.Parse(Console.ReadLine());
+
+            if (customer1.Age < 18)
+            {
+                Console.WriteLine("You're too young to have a bank account.");
+            }
+
+            else
+            {
+                //Instanciating a checking account
+                CheckingAccount checkingAccount = new CheckingAccount();
+
+                //settting properties for the checking account
+                checkingAccount.RoutingNumber = "654000457";
+                checkingAccount.AccountNumber = "2541234";
+                //***seting the owner proper to the instance of the customer we created
+                checkingAccount.Owner = customer1;
+
+                //printing the values of the account
+                Console.WriteLine($"Your checking account number is: {checkingAccount.AccountNumber}");
+
+                //Console.WriteLine($"The owner of the account is: {customer1.FirstName} {customer1.LastName}");
+                Console.WriteLine($"The owner of the account is: {checkingAccount.Owner.FirstName} {checkingAccount.Owner.LastName}");
+
+            }
+
         }
     }
 }
